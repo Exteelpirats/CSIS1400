ab5e5c4d8bd99eb53f0c78caaee803319891aafb
Exteelpirats <Exteelpirats@users.noreply.github.com>
2020-07-08 18:04:35 -0600

    Uploaded Java file required.

---
+++ PurchaseReport.java
@@ -0,0 +1,19 @@
+//Bryan Mendoza
+public class PurchaseReport{
+
+	public static void main(String []args){
+  
+		System.out.println("=========================");
+		System.out.print("Purchase Report\n");
+		System.out.println("=========================");
+		System.out.printf("%s\t\t  $%.2f\n", "Shirt", 21.99);
+		System.out.printf("%s\t\t  $%.2f\n", "Pants", 49.97);
+		System.out.printf("%s\t\t  $%.2f\n", "Shoes", 89.50);
+		System.out.printf("%s\t\t  $%.2f\n", "Belt", 19.99);
+		System.out.printf("%s\t\t $%.2f\n", "Coat", 129.95);
+		System.out.println("========================");
+		System.out.printf("%s\t\t $%.2f\n", "Total", 311.40);
+		
+	}
+
+}
