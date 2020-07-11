90bb16c0fdfcfd48441c38c088d420af0df72a62
Exteelpirats <38745861+Exteelpirats@users.noreply.github.com>
2020-07-11 14:23:53 -0600

    Update Week4Assignment.java

--- Week4Assignment.java
+++ Week4Assignment.java
@@ -10,30 +10,26 @@ public class Week4Assignment {
 		double height;
 		double width;
 		
-		
-		
 		System.out.print("This program will accept a length, width and height of a box, \n");
 		System.out.print("and then it will report the surface area and volume of it.\n\n");
-		
 		
 		Scanner input = new Scanner(System.in);
 		System.out.print("    " + "Enter the box length : ");
 		length = input.nextDouble();
-        System.out.print("    " + "Enter the box height : ");
-        height = input.nextDouble();
-        System.out.print("    " + "Enter the box width: ");
-        width = input.nextDouble();
-        calculateArea(length, height, width);
-        input.close();
+       		System.out.print("    " + "Enter the box height : ");
+		height = input.nextDouble();
+       		System.out.print("    " + "Enter the box width: ");
+       		width = input.nextDouble();
+        	calculateArea(length, height, width);
+       		input.close();
 	}
-    public static double calculateArea(double length, double height, double width) {
+     public static double calculateArea(double length, double height, double width) {
     	
-    	double SurfaceArea = (2 * (length * width + length * height + width * height));
-    	double Volume = (length * width * height);
-        System.out.printf("\nThe surface area of your rectangle is: %,.2f%n", SurfaceArea);
-        System.out.printf("The volume of your rectangle is: %,.2f%n", Volume);
+    		double SurfaceArea = (2 * (length * width + length * height + width * height));
+    		double Volume = (length * width * height);
+        	System.out.printf("\nThe surface area of your rectangle is: %,.2f%n", SurfaceArea);
+       		System.out.printf("The volume of your rectangle is: %,.2f%n", Volume);
 		return Volume;
-        
         
 	}
 }
